2d768144d54bb822904ef8f3bec51bc2b1e9a296
Danya <6694524@gmail.com>
2020-08-20 23:06:56 +0300

    added new site

--- components/header.js
+++ components/header.js
@@ -1,17 +1,19 @@
 'use strict';
 
 const header = () => {
-    const buttonOpenPanel = document.querySelector('.header-buttonWrapper-buttonOpenPanel');
+    const buttonOpenPanel = document.querySelector('.buttonWrapper');
     const navPanel = document.querySelector('.navPanel');
     buttonOpenPanel.addEventListener('click', () => {
         const pageWrapper = document.querySelector('.page-wrapper');
         pageWrapper.classList.toggle('pageWrapperBias');
         if (pageWrapper.classList.contains('pageWrapperBias')) {
             navPanel.classList.add('navPanelActive');
+            buttonOpenPanel.classList.add('pageWrapperBias');
         } else {
             navPanel.classList.remove('navPanelActive');
+            buttonOpenPanel.classList.remove('pageWrapperBias');
         }
-    })
-}
+    })    
+};
 
 export default header;
